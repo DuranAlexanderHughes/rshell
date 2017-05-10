175c54f7d0b6d13621cbdff62b1495f682d3b399
mister_bubbles <dhugh003@ucr.edu>
2017-05-10 02:28:19 +0000

    changed a test in multi_command.sh

--- tests/multi_command.sh
+++ tests/multi_command.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "ls -a; echo hello && mkdir test || echo world; git status" | bin/rshell
+echo "ls -a; echo hello || mkdir test && echo world" | bin/rshell
 #echo "ls -a; echo hello && mkdir test || echo world; git status;" | bin/rshell
 #echo "echo hello ; echo world" | bin/rshell
 #echo "echo hello || echo world" | bin/rshell 
